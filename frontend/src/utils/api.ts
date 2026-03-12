e43cee6ecede0bde5a59541ca2a9ea19679ddea7
Nikul <nikul.s.j@gmail.com>
2026-03-12 13:25:37 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://meals4-all.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
